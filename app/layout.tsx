7348a491fd9cf8ffdda637a2af5bf71fd613cb94
bikma <bikmasoft@gmail.com>
2024-08-23 09:26:01 +0530

    site Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           </div>
           {/* <TailwindIndicator /> */}
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
