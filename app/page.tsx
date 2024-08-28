af5f3beade1350919d8cdef1934207b1347ef763
bikma <bikmasoft@gmail.com>
2024-08-28 20:23:50 +0530

    Privacy Policy and Terms of Service

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <div className="flex items-center justify-center mt-[300px]">
+      <h1 className="text-2xl text-gray-500">In action soon</h1>
+    </div>
+  )
+}
