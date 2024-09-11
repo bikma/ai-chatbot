1c9ae2b71dee315223c391297232880d87e212a5
Prasad Siri <siri.prasad@gmail.com>
2024-09-12 05:06:17 +0530

    Testing husky one more time

--- scripts/add-path-comment.js
+++ scripts/add-path-comment.js
@@ -1,4 +1,3 @@
-// scripts\add-path-comment.js
 const fs = require('fs');
 const path = require('path');
 
@@ -9,18 +8,23 @@ const addPathComment = (filePath) => {
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) throw err;
 
+    // Check if the file already starts with the relative path comment
     if (!data.startsWith(comment)) {
       const updatedContent = comment + data;
       fs.writeFile(filePath, updatedContent, 'utf8', (err) => {
         if (err) throw err;
         console.log(`Added relative path to ${filePath}`);
       });
+    } else {
+      console.log(`Relative path already exists in ${filePath}, skipping...`);
     }
   });
 };
 
+// Get all staged files from command line arguments
 const stagedFiles = process.argv.slice(2);
 
+// Process only JS, TS, or TSX files
 stagedFiles.forEach((file) => {
   if (file.endsWith('.js') || file.endsWith('.ts') || file.endsWith('.tsx')) {
     addPathComment(file);
